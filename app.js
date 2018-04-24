04daf646d4b05f6ae55cf091b82f6842bd0751a4
milou <milouxp@gmail.com>
2018-04-24 11:37:16 +0900

    updated app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var handler = function(request, response) {
     console.log("Received request from " + request.connection.remoteAddress);
     response.writeHead(200);
     response.end("Your OS is " + os.platform() + "\n");
+    response.end("Your CPU is " + os.cpus()[0].model + "\n");
     response.end("You've hit " + os.hostname() + "\n");
 };
 
